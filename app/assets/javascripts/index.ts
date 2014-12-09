a0d106ca2c8b14745e6cc057855e536643ec2450
Isamu Mogi <isamu@leafytree.jp>
2014-12-09 16:43:13 +0900

    Initial implementation

---
+++ app/assets/javascripts/index.ts
@@ -0,0 +1,57 @@
+declare var $, GithubScouter, google
+google.load("visualization", "1", {packages:["corechart"]})
+
+$(() => {
+    function showChart(userName, powerLevel) {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['攻撃力', powerLevel.atk],
+            ['知力', powerLevel.int],
+            ['素早さ', powerLevel.agi]
+        ])
+        var width = $("#power_level_message").width() // this?
+        var options = {
+            sliceVisibilityThreshold: 0,
+            animation:{
+                duration: 1000,
+                easing: 'out'
+            },
+            width: width
+        }
+        $('#power_level_chart').width(width)
+
+        // What!?
+        var chart = new google.visualization.PieChart(document.getElementById('power_level_chart'))
+        chart.draw(data, options)
+    }
+
+    function showResult(userName, powerLevel) {
+        var message = '<span class="user-name">' + userName + '</span>さんのGitHub戦闘力は<br><span class="total-power-level">' +
+            "　" + powerLevel.total() + "</span>です。";
+        $("#power_level_message").html(message).show(300, () => { showChart(userName, powerLevel) })
+    }
+
+    function measure() {
+        $("#power_level_message").hide(200).empty()
+        $("#power_level_chart").empty()
+        $(".error-message").hide()
+        $(".measure-spinner").show()
+        var scouter = new GithubScouter()
+        var userName = $("#github_id").val()
+
+        scouter.measure(userName, (powerLevel) => {
+            console.log(powerLevel)
+            $(".measure-spinner").hide()
+            showResult(userName, powerLevel)
+        }, (e) => {
+            console.log(e)
+            $(".measure-spinner").hide()
+            $(".error-message").text(e.toString()).show()
+        })
+
+        return false
+    }
+
+    $("#measure").click(measure)
+    $("#github_id_form").submit(measure)
+})
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,11 @@
-logLevel := Level.Warn
+logLevel := Level.Info
 
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.6")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.7")
+
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.7")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-coffeescript" % "1.0.0")
+
+addSbtPlugin("com.arpnetworking" % "sbt-typescript" % "0.1.4")
