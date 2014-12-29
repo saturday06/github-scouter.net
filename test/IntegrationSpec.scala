2d4dfafdbddeea9a087ce41c9ff75bd7d41c9830
Isamu Mogi <isamu@leafytree.jp>
2014-12-30 03:32:46 +0900

    HtmlUnitのかわりにGhostDriverを使うようにした

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,9 +1,8 @@
+import org.junit.runner._
+import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.specs2.mutable._
 import org.specs2.runner._
-import org.junit.runner._
-
 import play.api.test._
-import play.api.test.Helpers._
 
 /**
  * add your integration spec here.
@@ -11,10 +10,11 @@ import play.api.test.Helpers._
  */
 @RunWith(classOf[JUnitRunner])
 class IntegrationSpec extends Specification {
+  def webDriver = WebDriverFactory(classOf[PhantomJSDriver])
 
   "Application" should {
 
-    "work from within a browser" in new WithBrowser {
+    "work from within a browser" in new WithBrowser(webDriver = webDriver) {
 
       browser.goTo("http://localhost:" + port)
 
